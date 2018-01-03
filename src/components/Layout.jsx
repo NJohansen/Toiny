e48a2e5b734e3361d4e648892f2c8c61f4f6f809
Victor Gram Thomsen <31857458+grammeaway@users.noreply.github.com>
2018-01-03 20:48:11 +0100

    Button component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import Header from 'components/Header'
+import Button from 'components/Button'
 export default ({ children }) => (
   <div>
     <Header/>
+    <Button> 
+      Cash out for the big G's 
+      </Button>
     {children}
   </div>
 )
